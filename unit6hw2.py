8e7f04cf9a708e1b6ccf7775aa329a8977d6b059
Duncan Staats <staatsd@wlhs.wlwv.k12.or.us>
2024-12-10 17:53:18 +0000

    mnews

--- unit6hw2.py
+++ unit6hw2.py
@@ -1,9 +1,24 @@
-def isInteger():
-    
+def isInteger(check):
+    #create a word bank
+    digits  = "+-0123456789"
+    ABCs = "abcdefghijklmnopqrstuvwxyz"
+    #strip the code of leadind and trailing white space
+    check = check.strip()
+    #check for digits and ABCs
+    for character in ABCs:
+        if character in check:
+            return False
+        
+    for character in digits:
+        if character in check:
+#return true and false 
+            return True
+    return False
+
 
 print(isInteger("123"))        
 print(isInteger("   -456  "))  
 print(isInteger("+789"))       
 print(isInteger("abc"))        
 print(isInteger("  "))         
-print(isInteger("-"))          
+print(isInteger("-ji"))          
